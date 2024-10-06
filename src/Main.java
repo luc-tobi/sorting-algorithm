4f5cbd73331d011b427f158a9cb48abd451686cf
tobi <luchnertobias@protonmail.com>
2024-10-06 20:55:13 +0200

    first step

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,29 @@
 package src;
+import java.util.Arrays;
 
 public class Main
 {
- public static void main(String[] args)
- {
-     
- }
+     public static void main(String[] args)
+     {
+         int[] numbers1 = {};
+         System.out.println("Array beginning:");
+         System.out.println(Arrays.toString(numbers1));
+
+         for (int i = 0; i < numbers1.length - 1; i++)
+         {
+             int num1 = numbers1[i];
+             System.out.println(num1);
+             int num2 = numbers1[i + 1];
+             System.out.println(num2);
+
+             if (num1 > num2)
+             {
+                 int num3 = numbers1[i];
+                 numbers1[i] = numbers1[i + 1];
+                 numbers1[i + 1] = num3;
+                 System.out.println("Array swapped:");
+                 System.out.println(Arrays.toString(numbers1));
+             }
+         }
+     }
 }
