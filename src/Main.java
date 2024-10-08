dafd4d7a90c5a8aec112f458074e559d23f12f22
tobi <luchnertobias@protonmail.com>
2024-10-08 17:43:05 +0200

    First working variant

--- src/Main.java
+++ src/Main.java
@@ -3,25 +3,35 @@ import java.util.Arrays;
 
 public class Main
 {
-     public static void main(String[] args)
+    public static void main (String[] args)
      {
-         int[] numbers1 = {};
-         System.out.println("Array beginning:");
+         int[] numbers1 = {22, 33, 10, 1, 5, 9};
+         int range = numbers1.length;
+         System.out.println("Your unsorted input array:");
          System.out.println(Arrays.toString(numbers1));
+         boolean swap = true;
 
-         for (int i = 0; i < numbers1.length - 1; i++)
+         while (swap)
          {
-             int num1 = numbers1[i];
-             System.out.println(num1);
-             int num2 = numbers1[i + 1];
-             System.out.println(num2);
 
-             if (num1 > num2)
+             swap =false;
+             for (int i = 0; i < range -1; i++)
              {
-                 int num3 = numbers1[i];
-                 numbers1[i] = numbers1[i + 1];
-                 numbers1[i + 1] = num3;
-                 System.out.println("Array swapped:");
+
+                 int num1 = numbers1[i];
+                 int num2 = numbers1[i + 1];
+
+                 if (num1 > num2)
+                 {
+                     int num3 = numbers1[i];
+                     numbers1[i] = numbers1[i + 1];
+                     numbers1[i + 1] = num3;
+                     swap = true;
+                 }
+             }
+             if (swap == false)
+             {
+                 System.out.println("Your array ist sorted! Here is your array:");
                  System.out.println(Arrays.toString(numbers1));
              }
          }
